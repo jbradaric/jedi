0959237f43165c802526e016fd18a7fc91ca7259
Jurica Bradaric <jurica.bradaric@avl.com>
2019-02-28 07:53:06 +0100

    xxx

--- jedi/transform_defslot_nopy.cpp
+++ jedi/transform_defslot_nopy.cpp
@@ -2,23 +2,24 @@
 #include <tuple>
 #include <cctype>
 #include <algorithm>
+#include <string_view>
 
 namespace transform {
 
 static const std::string DEFSLOT("defslot(");
 static const std::string DEFCONST("defconst(");
+static const std::string DEFVIRTSLOT("defvirtslot(");
 static const std::string TYPE_STMT("type=");
 
 enum class StatementType {
     defslot,
+    defvirtslot,
     defconst
 };
 
-typedef std::tuple<std::string::const_iterator, std::string::const_iterator> iter_tuple;
-
-std::tuple<size_t, size_t> find_end(const std::string& source, size_t start) {
-    size_t num_newlines = 0;
-    size_t num_parens = 1;
+auto find_end(const std::string_view source, size_t start) {
+    auto num_newlines = 0;
+    auto num_parens = 1;
 
     auto it = begin(source) + start;
     auto end_iter = end(source);
@@ -36,45 +37,40 @@ std::tuple<size_t, size_t> find_end(const std::string& source, size_t start) {
         }
     }
 
-    return std::make_tuple(num_newlines,
-                           it - begin(source));
+    return std::make_tuple(num_newlines, it - begin(source));
 }
 
-iter_tuple extract_type(std::string::const_iterator type_begin,
-                        std::string::const_iterator stmt_end) {
+template<typename Iterator>
+auto extract_type(Iterator type_begin, Iterator stmt_end) {
     auto type_end = type_begin;
-    while (type_end != stmt_end
-           && *type_end != ','
-           && *type_end != ')') {
+    while (type_end != stmt_end && *type_end != ',' && *type_end != ')') {
         ++type_end;
     }
     return std::make_tuple(type_begin, type_end);
 }
 
-void append_init(std::string::const_iterator stmt_begin,
-                 std::string::const_iterator stmt_end,
-                 std::string& result) {
+template <typename Iterator>
+void append_init(Iterator stmt_begin, Iterator stmt_end, std::string& result) {
     auto type_start = std::search(stmt_begin, stmt_end, begin(TYPE_STMT), end(TYPE_STMT));
     if (type_start == stmt_end) {
         result.append(" = None");
         return;
     }
 
-    std::string::const_iterator it1, it2;
-    std::tie(it1, it2) = extract_type(type_start + TYPE_STMT.size(), stmt_end);
+    auto [it1, it2] = extract_type(type_start + TYPE_STMT.size(), stmt_end);
     result.append(" = ");
     result.append(it1, it2);
     result.append("()");
 }
 
-std::string transform_single(const std::string& source, size_t start,
-                             StatementType stmt_type, size_t& pos) {
-    size_t defstmt_len = 0;
-    if (stmt_type == StatementType::defslot) {
-        defstmt_len = DEFSLOT.size();
-    } else {
-        defstmt_len = DEFCONST.size();
-    }
+auto transform_single(const std::string_view source, size_t start,
+                      StatementType stmt_type, size_t& pos) {
+    static size_t statement_lengths[] = {
+        DEFSLOT.size(),
+        DEFVIRTSLOT.size(),
+        DEFCONST.size(),
+    };
+    size_t defstmt_len = statement_lengths[static_cast<int>(stmt_type)];
 
     size_t num_newlines;
     size_t stmt_end;
@@ -107,15 +103,17 @@ std::string transform_single(const std::string& source, size_t start,
     }
 
     result.append(it, name_iter);
-    append_init(name_iter, begin(source) + stmt_end, result);
+    if (stmt_type == StatementType::defvirtslot)
+        result.append(" = None");
+    else
+        append_init(name_iter, begin(source) + stmt_end, result);
     result.append(num_newlines, '\n');
 
     pos = stmt_end;
-
     return result;
 }
 
-std::string transform_source(const std::string& source) {
+std::string transform_source(const std::string_view source) {
     std::string result;
     result.reserve(source.size());
 
@@ -155,7 +153,7 @@ std::string transform_source(const std::string& source) {
 extern "C" {
 
 char *transform_source(const char *source, unsigned long length) {
-    std::string result = transform::transform_source(std::string(source, length));
+    std::string result = transform::transform_source(std::string_view(source, length));
     char *transformed = new char[result.size() + 1];
     result.copy(transformed, result.size());
     transformed[result.size()] = '\0';
